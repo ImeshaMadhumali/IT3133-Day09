a489f07533c150414c127fb72489045ab45b0d34
Imesha Madhumali <imeshamadhumali31@gmail.com>
2024-12-18 19:31:59 +0530

    Create the simple index

--- app/index.tsx
+++ app/index.tsx
@@ -1,4 +1,4 @@
-import { Text, View } from "react-native";
+import { Image, StyleSheet,Text, View } from "react-native";
 
 export default function Index() {
   return (
@@ -9,7 +9,23 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Image source={require("../assets/images/react-logo.png")} style={styles.logo}/>
+      <Text style={styles.name}>Hello World!</Text>
+      <Text>My first React Native cross platform app.</Text>
     </View>
   );
 }
+
+const styles=StyleSheet.create({
+  logo:{
+    width:100,
+    height:100,
+    borderStyle:"solid",
+    borderWidth:3,
+    borderColor:"blue"
+  },
+  name:{
+   fontSize: 20,
+   fontStyle: "italic"
+  }
+})
